17753f1cc900ebd66c7a86aa8c6028353c26a5b5
hecter_wu <hecter_wu@transcend.com.tw>
2017-05-12 17:48:50 +0800

    pass testcase: testSecondLevelBST by DFS

--- Convert-BST-to-Greater-Tree.py
+++ Convert-BST-to-Greater-Tree.py
@@ -6,12 +6,30 @@ class unitest(unittest.TestCase):
         row = []
         tree = TreeSolution.TreeSolution()
         root = tree.AddBinaryTreeNode(row)
-        self.assertEqual(Solution().convertBST(root),[]);
+        self.assertEqual(Solution().convertBST(root),[])
+    def testSecondLevelBST(self):
+        row = [5,2,13]
+        tree = TreeSolution.TreeSolution()
+        root = tree.AddBinaryTreeNode(row)
+        Solution().convertBST(root)
+        self.assertEqual(root.val,18)
+        self.assertEqual(root.left.val,20)
+        self.assertEqual(root.right.val,13)
 
 class Solution(object):
+    def GreaterTree(self,root,num):
+        if not root:
+            return 0
+        add = 0
+        if root.right:
+            add =  self.GreaterTree(root.right,0)
+        root.val += (add+num)
+        if root.left:
+            self.GreaterTree(root.left,root.val)
+        return root.val
     def convertBST(self, root):
         if not root:
             return []
-
+        self.GreaterTree(root,0)
 if __name__ == '__main__':
     unittest.main()
